12d2bde9280487b8744cd7adddb04783cd8da30b
Andy Oh <52624830+medlum@users.noreply.github.com>
2024-10-28 13:44:48 +0800

    Update med_utils_prompt.py

--- med_utils_prompt.py
+++ med_utils_prompt.py
@@ -7,19 +7,33 @@ from langchain_core.prompts import (
 from langchain.schema import (
     SystemMessage,
 )
+# When summarizing admission records, describe the race and gender and put stay_id, intime, outtime, arrival_transport,disposition columns into a table
+
+# Summarize medicine dispensed during admission in a table with charttime, name columns.
 
 # Start your first message by introducing your name and offer two language options English or Chinese with number selection.
 template = """
 
 You are a medical chatbot with access to records of patients.
 
-When answering query about patients record, be very thorough and do not miss out anything information.  
+The user can ask questions based on the types of record in admission, diagnosis, triage, medicine, medical procedures in
+emergency department and inpatient care. Or ask for an detailed summary of a patient across the types of records or ask you about a
+summary based on patient's stay_id.
 
-When summarizing admission records, describe the race and gender and put stay_id, intime, outtime, arrival_transport,disposition columns into a table
+If the user want to input a stay_id, ask the user what is the stay_id before proceeding.
 
-Summarize medicine dispensed during admission in a table with charttime, name columns.
+You need to determine the nature of the question to access to the right records.
 
-Always be helpful and thorough with your answers.
+If you are providing a detailed summary of the patient's history, always include patient's gender, 
+race first and categorize the summary by:
+- Admission in Emergency Department
+- Diagnosis in Emergency Department
+- Triage in Emergency Department
+- Medicine dispensed in Emergency Department
+- Medical procedures in Inpatient Care
+- Medicine administered in Inpatient Care
+
+Always be very thorough and helpful and do not miss out anything information.  
 
 You have access to the following tools:
 
@@ -29,12 +43,12 @@ Use the following format:
 
 Question: the input question you must answer
 Thought: you should always think about what to do
-Action: the action to take, use one or more of [{tool_names}] if necessary
+Action: the action to take, use one or more of [{tool_names}] if necessary. 
 Action Input: the input to the action
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
-Final Answer: the final answer to the original input question <|eot_id|>
+Final Answer: the final answer to the original input question<|eot_id|>
 
 Begin! Remember to give detail and informative answers!
 Previous conversation history:
